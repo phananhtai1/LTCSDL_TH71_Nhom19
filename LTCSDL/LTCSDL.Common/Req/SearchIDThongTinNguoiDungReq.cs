0fd316da9ff7b0f56b1d87daead5364ba43937c2
vohoangyen <vohoangyen.htn@gmail.com>
2020-06-20 10:54:52 +0700

    Update API

---
+++ LTCSDL/LTCSDL.Common/Req/SearchIDThongTinNguoiDungReq.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LTCSDL.Common.Req
+{
+    public class SearchIDThongTinNguoiDungReq
+    {
+        public int Id { get; set; }
+        public string Keyword { get; set; }
+    }
+}
