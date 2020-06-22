ab56a16e88c70dc4f29bcab4fa7306c329e6c3c8
phananhtai1 <1751010128tai@ou.edu.vn>
2020-06-22 22:52:25 +0700

    Them angular

--- LTCSDL/LTCSDL.Web/ClientApp/src/app/app.module.ts
+++ LTCSDL/LTCSDL.Web/ClientApp/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
+import {CookieService} from 'ngx-cookie-service';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ITCompaniesComponent } from './itcompanies/itcompanies.component';
-
+import { ApplyComponent } from './apply/apply.component';
+import { SearchComponent } from './search/search.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,6 +20,8 @@ import { ITCompaniesComponent } from './itcompanies/itcompanies.component';
     CounterComponent,
     FetchDataComponent,
     ITCompaniesComponent,
+    ApplyComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -29,9 +32,11 @@ import { ITCompaniesComponent } from './itcompanies/itcompanies.component';
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'itcompanies', component: ITCompaniesComponent },
+      { path: 'apply', component: ApplyComponent },
+      { path: 'search', component: SearchComponent },
     ])
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- LTCSDL/LTCSDL.Web/ClientApp/src/app/itcompanies/itcompanies.component.ts
+++ LTCSDL/LTCSDL.Web/ClientApp/src/app/itcompanies/itcompanies.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {CookieService} from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-itcompanies',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ITCompaniesComponent implements OnInit {
 
-  constructor() { }
+  public x:any ={
+    page:1,
+    size: 20,
+    keyword:""
+  };
 
-  ngOnInit() {
+  constructor(
+    private http: HttpClient, @Inject('BASE_URL') baseUrl: string,
+    private cookie:CookieService,
+    private router: Router) { }
+
+  public TTNTD : ttntd ={
+    maCongTy: "",
+    tenCongTy: "",
+    diaChi: "",
+    thanhPho: "",
+    soDienThoai: "",
+    email: "",
+    thongTinTuyenDung: []
   }
+  public list : ttntd[] = []; 
+  ngOnInit() {
+    this.http.post('https://localhost:44368/api/TTNhaTuyenDung/search-thongtin-nhatuyendung' , this.x).subscribe(result =>{
+      var res : any;
+      res = result;
+      var y : ttntd ={
+        maCongTy: "",
+        tenCongTy: "",
+        diaChi: "",
+        thanhPho: "",
+        soDienThoai: "",
+        email: "",
+        thongTinTuyenDung: []
+      }
+      y = res.data.data;
+      this.TTNTD = y;
+      console.log(y);
+      console.log(this.TTNTD);
+      this.list.push(this.TTNTD);
+    })
+    
+  } 
 
+
+}
+
+interface ttntd{
+  maCongTy: string,
+  tenCongTy: string,
+  diaChi: string,
+  thanhPho: string,
+  soDienThoai: string,
+  email: string,
+  thongTinTuyenDung: []
 }
